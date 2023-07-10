a32f169b3e75d598ee843af5a3f20f84bff458cf
fabba1892 <72152804+fabba1892@users.noreply.github.com>
2023-07-10 20:42:44 +0000

    updated secrets

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+const accountSid = process.env['TWILIO_ACCOUNT_SID'];
+const authToken = process.env['TWILIO_AUTH_TOKEN'];
 const client = require('twilio')(accountSid, authToken);
 
 console.log("Starting Program")
