84304ade60376a2e27d1254e60aaedf4ef6ae437
fabba1892 <72152804+fabba1892@users.noreply.github.com>
2023-07-10 11:44:41 +0000

    twilio api testing

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-client.messages.list({limit: 20})
-               .then(messages => messages.forEach(m => console.log(m.sid)));
+client.messages.list({ limit: 20 })
+  .then(messages => messages.forEach(m => console.log(m.sid)));
